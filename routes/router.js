ce512475ebd5267d8a4bf8f56ba0e68f5fdea03d
kmtovar <km.tovar@pm.me>
2022-06-29 15:15:27 -0400

    last min updates

--- routes/router.js
+++ routes/router.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 // Signup
-
 router.get('/signup', (req,res) => res.render('signup', {title: 'Sign Up'}));
+
+// Login
+router.get('/login', (req, res) => res.render('login', {title: 'Log In'}))
 
 module.exports = router;
